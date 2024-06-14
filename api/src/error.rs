7a411f6263e94c328099cbd1830c963cfd169870
marco <toyko2001@gmail.com>
2024-06-14 16:00:46 +0630

    ping route

---
+++ api/src/error.rs
@@ -0,0 +1,12 @@
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {}
+
+impl std::fmt::Display for Error {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{self:?}")
+  }
+}
+
+impl std::error::Error for Error {}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("Hello, world!");
+use axum::{response::IntoResponse, routing::get, Router};
+use reqwest::StatusCode;
+use tokio::net::TcpListener;
+
+use self::error::Result;
+
+mod error;
+
+async fn ping_handler() -> impl IntoResponse {
+  (StatusCode::OK, "PONG")
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  let routers = Router::new().route("/ping", get(ping_handler));
+
+  let tcp_listener = TcpListener::bind("localhost:8000").await.unwrap();
+  axum::serve(tcp_listener, routers.into_make_service())
+    .await
+    .unwrap();
+
+  Ok(())
 }
